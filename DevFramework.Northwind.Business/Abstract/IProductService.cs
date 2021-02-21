5810993fa933746b24c4e292de8866fe041e3cba
Mustafa <mustafaserifcelenk@gmail.com>
2021-02-21 16:29:55 +0300

    Business Layer Implementation

---
+++ DevFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    public interface IProductService //sistemi managera bağlı kılmıyoruz böylece wcf entegre ettiğimizde sistemi wcf üzerinden de servis edebileceğiz
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product); 
+    }
+}
---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,37 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+            //EfProductDal efProductDal = new EfProductDal(); Böyle yaparsan businessı efye bağımlı hale getirirsin
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.productId == id);
+        }
+    }
+}
