89d64e08db53ccdd94380a64c22f012998b6315d
Mustafa <mustafaserifcelenk@gmail.com>
2021-02-21 10:19:24 +0300

    Interfaces are created

---
+++ DevFramework.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T:class, IEntity, new()
+    {
+        IQueryable<T> Table { get; }
+
+        //Bir contexte attach olarak o context kapanmadan önce(mesela bir tolist() işleminden sonra o satırdaki sorgu kapanıyor oluyor) o contexte bağlı olaraktan gerekli operasyonlarımızı yapıyor olacağız
+    }
+}
