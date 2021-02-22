0a27ae33d5bfbe0919660996c7183ec73ea501ae
Mustafa <mustafaserifcelenk@gmail.com>
2021-02-22 13:49:22 +0300

    Loggin Intro

---
+++ DevFramework.Core/CrossCuttingConcerns/Logging/LogDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging
+{
+    public class LogDetail
+    {
+        public string FullName { get; set; } //Namespace ismi
+        public string MethodName { get; set; } // O Namespacedeki methodun ismi(update vs)
+        public List<LogParameter> Paramaters { get; set; } //Methodun parametreleri birden fazla olabilir
+    }
+}
---
+++ DevFramework.Core/CrossCuttingConcerns/Logging/LogParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging
+{
+    public class LogParameter
+    {
+        public string Name { get; set; } 
+        public string Type { get; set; }
+        public object Value { get; set; }
+        //Mesela updatedeki product için ismi product, tipi Product, içeriği productta ne varsa, bunları LogParameter içinde tutacağız
+    }
+}
