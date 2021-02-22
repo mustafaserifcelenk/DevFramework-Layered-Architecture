0c0d7d4920c157f800cb2e44faddb04284f2ed3b
Mustafa <mustafaserifcelenk@gmail.com>
2021-02-22 17:24:23 +0300

    File logging entegration to Database

---
+++ DevFramework.Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/FileLogger.cs
@@ -0,0 +1,17 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class FileLogger : LoggerService
+    {
+        //Dosayaya yazacak logger oluşturulması
+        public FileLogger() : base(LogManager.GetLogger("JsonFileLogger"))
+        {
+        }
+    }
+}
