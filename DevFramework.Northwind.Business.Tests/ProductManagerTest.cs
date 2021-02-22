d452e55ddccffdd60fd3fdde9204e4d479e25827
Mustafa <mustafaserifcelenk@gmail.com>
2021-02-22 07:56:33 +0300

    Test Business ? MOCK

---
+++ DevFramework.Northwind.Business.Tests/ProductManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Northwind.Business.Concrete.Managers;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace DevFramework.Northwind.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTest
+    {
+        [ExpectedException(typeof(ValidationException))] 
+        [TestMethod]
+        public void Product_validation_check()
+        {
+            Mock<IProductDal> mock = new Mock<IProductDal>(); //Test için IProductDal'ın concreteine ihtiyacımız var, Mock toolu bunu sağlıyor bize
+            ProductManager productManager = new ProductManager(mock.Object);
+
+            productManager.Add(new Product()); //Kurallara aykırı olarak her şeyi boş gönderdik
+        }
+    }
+}
