31735f1eb747952523d906138c696b2df0d42c4a
Mustafa <mustafaserifcelenk@gmail.com>
2021-02-23 07:25:17 +0300

    Catching Errors and Logging Them

--- DevFramework.Northwind.Mvc.WebUI/Properties/AssemblyInfo.cs
+++ DevFramework.Northwind.Mvc.WebUI/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using DevFramework.Core.Aspects.Postsharp.ExceptionAspects;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,6 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 //[assembly: LogAspect(typeof(FileLogger), AttributeTargetTypes = "DevFramework.Northwind.Business.Concrete.Managers.*")] Buraya log yazarsan tüm managerlar etkilenir, veya attributeu kullanarak belli bir dosyadaki tüm managerları loglarsın ve ya get ile başlayanları (.Get*), veya içinde Add geçenleri(.*Add*)
+//[assembly: ExceptionLogAspect("")] yukardakiyle aynı
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
